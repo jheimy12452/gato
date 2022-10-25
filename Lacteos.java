18c5e1687a5616ee2ea5b0bb3f2001cbc1ea344e
jheimy12452 <115821755+jheimy12452@users.noreply.github.com>
2022-10-25 11:12:46 -0400

    Create Lacteos.java

---
+++ Lacteos.java
@@ -0,0 +1,19 @@
+public abstract class Pokemon {
+    int numPokedex;
+    String nombre;
+    double peso;
+    String sexo;
+    int temporada;
+
+    //metodos
+    public abstract void atacarPlacaje();
+    public abstract void atacarAranazo();
+    public abstract void atacarMordisco();
+
+
+
+
+
+
+
+}
